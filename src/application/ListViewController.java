09914abd9e5d2cef3ba1e6014632e8124704bc01
Piotr Skonieczny <piotr.skonieczny@gmail.com>
2018-06-02 17:30:05 +0200

    files opearations

--- src/application/ListViewController.java
+++ src/application/ListViewController.java
@@ -11,17 +11,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
@@ -29,8 +27,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DataFormat;
 import javafx.scene.input.Dragboard;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
@@ -40,17 +36,19 @@ public class ListViewController implements Initializable {
 	private static final String START_PATH = "C:\\Users\\Asus\\Downloads";
 	private static final String TABLE_VIEW_1_ID = "tableView1";
 	private static final String TABLE_VIEW_2_ID = "tableView2";
-	private static final String TABLE_VIEW_ID_KEY = "ID";
 
 	private static final DataFormat SERIALIZED_MIME_TYPE = new DataFormat("application/x-java-serialized-object");
 
 	private StringBuilder currentPath1 = new StringBuilder(START_PATH);
 	private StringBuilder currentPath2 = new StringBuilder(START_PATH);
-	private boolean copyOn = false;
-	private final ObjectProperty<ListCell<String>> dragSource = new SimpleObjectProperty<>();
+
+	private List<File> cachedFiles = new ArrayList<>();
 
 	@FXML
 	private Pane pane1;
+
+	@FXML
+	private SplitPane splitPane1;
 
 	@FXML
 	private Label pathLabel1;
@@ -90,37 +88,17 @@ public class ListViewController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		setupResize();
+
 		mapTableViewColumns();
-		
+
 		tableView1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		tableView2.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-		
+
 		setupDragAndDrop(tableView1);
-		setupDragAndDrop(tableView2);		
-		
-		listStartDirectory();		
-	}
+		setupDragAndDrop(tableView2);
 
-	public void handleKeyPressedPane1(KeyEvent event) {
-		KeyCode keyCode = event.getCode();
-		if (keyCode == KeyCode.C) {
-			copyOn = true;
-		}
-	}
-
-	public void handleKeyReleasedPane1(KeyEvent event) {
-		KeyCode keyCode = event.getCode();
-		if (keyCode == KeyCode.C) {
-			copyOn = false;
-		}
-	}
-
-	public void handleKeyPressedListView1(KeyEvent event) {
-		// delete(event, tableView1, currentPath1);
-	}
-
-	public void handleKeyPressedListView2(KeyEvent event) {
-		// delete(event, tableView2, currentPath2);
+		listStartDirectory();
 	}
 
 	public void handleMouseClickTableView1(MouseEvent event) {
@@ -139,16 +117,47 @@ public class ListViewController implements Initializable {
 		goUpDirectory(tableView2, currentPath2);
 	}
 
-	private void delete(KeyEvent event, ListView<String> listView, StringBuilder currentPath) {
-		KeyCode keyCode = event.getCode();
-		if (keyCode == KeyCode.DELETE) {
-			String selectedItem = listView.getSelectionModel().getSelectedItem();
-			File selectedFile = new File(currentPath + "\\" + selectedItem);
-			if (selectedFile.isFile()) {
-				selectedFile.delete();
-				// listSelectedDirectory(listView, currentPath);
+	public void handleActionDelete(ActionEvent event) throws IOException {
+		for (FileRow item : tableView1.getSelectionModel().getSelectedItems()) {
+			removeOnDisk(item.getFileName(), TABLE_VIEW_1_ID);
+		}
+		for (FileRow item : tableView2.getSelectionModel().getSelectedItems()) {
+			removeOnDisk(item.getFileName(), TABLE_VIEW_2_ID);
+		}
+
+		listSelectedDirectory(tableView1, currentPath1);
+		listSelectedDirectory(tableView2, currentPath2);
+	}
+
+	public void handleActionCopy(ActionEvent event) {
+		cachedFiles.clear();
+
+		for (FileRow item : tableView1.getSelectionModel().getSelectedItems()) {
+			cachedFiles.add(new File(currentPath1.toString() + "\\" + item.getFileName()));
+		}
+		for (FileRow item : tableView2.getSelectionModel().getSelectedItems()) {
+			cachedFiles.add(new File(currentPath2.toString() + "\\" + item.getFileName()));
+		}
+	}
+
+	public void handleActionPaste(ActionEvent event) throws IOException {
+		if (tableView1.isFocused()) {
+			for (File file : cachedFiles) {
+				copyOnDisk(file, currentPath1.toString());
+			}
+		} else if (tableView2.isFocused()) {
+			for (File file : cachedFiles) {
+				copyOnDisk(file, currentPath2.toString());
 			}
 		}
+
+		listSelectedDirectory(tableView1, currentPath1);
+		listSelectedDirectory(tableView2, currentPath2);
+		cachedFiles.clear();
+	}
+
+	private void setupResize() {
+
 	}
 
 	private void mapTableViewColumns() {
@@ -167,8 +176,10 @@ public class ListViewController implements Initializable {
 		for (File file : directory.listFiles()) {
 			startDirectoryElements.add(new FileRow(file.getName(), file.length(), file.lastModified()));
 		}
+
 		tableView1.setItems(FXCollections.observableArrayList(startDirectoryElements));
 		tableView2.setItems(FXCollections.observableArrayList(startDirectoryElements));
+
 		pathLabel1.setText(START_PATH);
 		pathLabel2.setText(START_PATH);
 	}
@@ -210,10 +221,11 @@ public class ListViewController implements Initializable {
 
 	private void listSelectedDirectory(TableView<FileRow> listView, StringBuilder currentPath) {
 		List<FileRow> startDirectoryElements = new ArrayList<>();
-		// startDirectoryElements.add(RETURN_ITEM);
 		File directory = new File(currentPath.toString());
 		for (File file : directory.listFiles()) {
-			startDirectoryElements.add(new FileRow(file.getName(), file.length(), file.lastModified()));
+			if (file.exists()) {
+				startDirectoryElements.add(new FileRow(file.getName(), file.length(), file.lastModified()));
+			}
 		}
 		listView.setItems(FXCollections.observableArrayList(startDirectoryElements));
 	}
@@ -253,15 +265,15 @@ public class ListViewController implements Initializable {
 			if (db.hasContent(SERIALIZED_MIME_TYPE)) {
 				List<Integer> draggedIndex = (List<Integer>) db.getContent(SERIALIZED_MIME_TYPE);
 				String tableViewId = (String) db.getContent(DataFormat.PLAIN_TEXT);
-				
+
 				TableView<FileRow> currentTuble = getCurrentTable(tableViewId);
-				ObservableList<FileRow> items =currentTuble.getItems();
+				ObservableList<FileRow> items = currentTuble.getItems();
 				String oppositeTableId = getOppositeTable(tableViewId).getId();
-				
-				for(int idx : draggedIndex) {
+
+				for (int idx : draggedIndex) {
 					moveOnDisk(items.get(idx).getFileName(), oppositeTableId);
 				}
-				
+
 				listSelectedDirectory(tableView1, currentPath1);
 				listSelectedDirectory(tableView2, currentPath2);
 
@@ -272,10 +284,10 @@ public class ListViewController implements Initializable {
 	}
 
 	private void moveOnDisk(String fileName, String destinationListViewId) {
-		if(currentPath1.toString().equals(currentPath2.toString())) {
+		if (currentPath1.toString().equals(currentPath2.toString())) {
 			return;
 		}
-		
+
 		if (TABLE_VIEW_1_ID.equals(destinationListViewId)) {
 			Path currentFilePath = FileSystems.getDefault().getPath(currentPath2 + "\\" + fileName);
 			Path destinationFilePath = FileSystems.getDefault().getPath(currentPath1 + "\\" + fileName);
@@ -293,37 +305,36 @@ public class ListViewController implements Initializable {
 		}
 	}
 
-	private void copyOnDisk(String fileName, String destinationListViewId) {
+	private void copyOnDisk(File file, String destinationPath) throws IOException {
+		Files.copy(FileSystems.getDefault().getPath(file.getCanonicalPath()),
+				FileSystems.getDefault().getPath(destinationPath + "\\" + file.getName()),
+				StandardCopyOption.REPLACE_EXISTING);
+	}
+
+	private void removeOnDisk(String fileName, String destinationListViewId) throws IOException {
 		if (TABLE_VIEW_1_ID.equals(destinationListViewId)) {
-			Path currentFilePath = FileSystems.getDefault().getPath(currentPath2 + "\\" + fileName);
-			Path destinationFilePath = FileSystems.getDefault().getPath(currentPath1 + "\\" + fileName);
-			try {
-				Files.copy(currentFilePath, destinationFilePath, StandardCopyOption.REPLACE_EXISTING);
-			} catch (IOException exeption) {
-			}
-		} else if (TABLE_VIEW_2_ID.equals(destinationListViewId)) {
 			Path currentFilePath = FileSystems.getDefault().getPath(currentPath1 + "\\" + fileName);
-			Path destinationFilePath = FileSystems.getDefault().getPath(currentPath2 + "\\" + fileName);
-			try {
-				Files.copy(currentFilePath, destinationFilePath, StandardCopyOption.REPLACE_EXISTING);
-			} catch (IOException exeption) {
-			}
+			Files.delete(currentFilePath);
+		}
+		if (TABLE_VIEW_2_ID.equals(destinationListViewId)) {
+			Path currentFilePath = FileSystems.getDefault().getPath(currentPath2 + "\\" + fileName);
+			Files.delete(currentFilePath);
 		}
 	}
-	
+
 	private TableView<FileRow> getCurrentTable(String id) {
-		if(TABLE_VIEW_1_ID.equals(id)) {
+		if (TABLE_VIEW_1_ID.equals(id)) {
 			return tableView1;
 		}
-		
+
 		return tableView2;
 	}
-	
+
 	private TableView<FileRow> getOppositeTable(String id) {
-		if(TABLE_VIEW_1_ID.equals(id)) {
+		if (TABLE_VIEW_1_ID.equals(id)) {
 			return tableView2;
 		}
-		
+
 		return tableView1;
 	}
 }
